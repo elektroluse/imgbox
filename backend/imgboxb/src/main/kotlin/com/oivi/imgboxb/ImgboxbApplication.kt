c71db3d99dbe3a9226ca31e9f80d45889e30d0b5
elektroluse <pinkbean@hotmail.no>
2024-11-14 20:35:57 +0100

    backend initial commit

---
+++ backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/ImgboxbApplication.kt
@@ -0,0 +1,11 @@
+package com.oivi.imgboxb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImgboxbApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImgboxbApplication>(*args)
+}
---
+++ backend/imgboxb/src/test/kotlin/com/oivi/imgboxb/ImgboxbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oivi.imgboxb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImgboxbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

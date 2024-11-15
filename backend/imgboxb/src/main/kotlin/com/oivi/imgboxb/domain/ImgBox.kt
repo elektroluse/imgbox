a1f1024f0fe099a8f9f98158843d9216eda535e4
elektroluse <pinkbean@hotmail.no>
2024-11-15 17:51:16 +0100

    Added adminer service to docker-compose and timestamp property in ImgBox

--- backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/domain/ImgBox.kt
+++ backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/domain/ImgBox.kt
@@ -1,9 +1,12 @@
 package com.oivi.imgboxb.domain
 
+import java.sql.Timestamp
+
 data class ImgBox(
     val id : Long?,
     val title : String,
     val description : String,
     var tags : MutableSet<String>,
-    val user : User
+    val user : User,
+    val createdAt : Timestamp
     )

ae3b1be7dc18e662ee7ea4f09196399f6d7e39f8
elektroluse <pinkbean@hotmail.no>
2024-11-15 18:36:04 +0100

    JPA annotations on the entities

--- backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/domain/ImgBox.kt
+++ backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/domain/ImgBox.kt
@@ -1,12 +1,30 @@
 package com.oivi.imgboxb.domain
 
+import jakarta.persistence.*
+
 import java.sql.Timestamp
 
+@Entity(name = "imgboxes")
 data class ImgBox(
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
     val id : Long?,
+
+    @Column(name = "title")
     val title : String,
+
+    @Column(name = "description")
     val description : String,
+
+    @Column(name = "tags")
     var tags : MutableSet<String>,
+
+    @ManyToOne(cascade = [CascadeType.DETACH])
+    @JoinColumn(name = "user_id")
     val user : User,
+
+    @Column(name = "created_at")
     val createdAt : Timestamp
     )
--- backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/domain/User.kt
+++ backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/domain/User.kt
@@ -1,6 +1,17 @@
 package com.oivi.imgboxb.domain
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name="users")
 data class User(
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+    val id : Long?,
+
+    @Column(name="username", unique = true)
     val username : String
-    )
+
+)

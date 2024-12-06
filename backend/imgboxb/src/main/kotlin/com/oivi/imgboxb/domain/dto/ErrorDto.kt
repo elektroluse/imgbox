6afbfc09b5ba234a01c9a543ee79820638b63878
elektroluse <pinkbean@hotmail.no>
2024-12-06 21:29:45 +0100

    ErrorDto in FilterExceptionHandler

---
+++ backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/domain/dto/ErrorDto.kt
@@ -0,0 +1,9 @@
+package com.oivi.imgboxb.domain.dto
+
+import java.sql.Timestamp
+
+data class ErrorDto(
+    val message : String,
+    val timestamp : Timestamp
+
+)
--- backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/exceptions/FilterExceptionHandler.kt
+++ backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/exceptions/FilterExceptionHandler.kt
@@ -1,18 +1,23 @@
 package com.oivi.imgboxb.exceptions
 
+import com.oivi.imgboxb.domain.dto.ErrorDto
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.sql.Timestamp
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @RestControllerAdvice
 class FilterExceptionHandler {
 
     @ExceptionHandler(AuthenticationCredentialsNotFoundException::class)
     fun handleExpiredTokenException(
-        e : AuthenticationCredentialsNotFoundException) : ResponseEntity<String>{
-
-        return ResponseEntity<String>("Token is invalid or expired", HttpStatus.UNAUTHORIZED)
+        e : AuthenticationCredentialsNotFoundException) : ResponseEntity<ErrorDto>{
+        val errorDto = ErrorDto("Token is invalid or expired", Timestamp.valueOf(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)))
+        return ResponseEntity<ErrorDto>(errorDto, HttpStatus.UNAUTHORIZED)
     }
 }

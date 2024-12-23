c38f9d08c61a58cfee75cd1e4e0fd4e64c688acd
elektroluse <pinkbean@hotmail.no>
2024-12-23 21:22:38 +0100

    error and loading state for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,19 +8,38 @@ const BASE_URL = 'http://localhost:8080/api/v1'
 
 function App() {
 
-  
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const [users, setUsers] = useState<UserInfo[]>([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const response = await fetch(`${BASE_URL}/users`, {method : 'get'});
-      const users = (await response.json()) as UserInfo[];
-      setUsers(users);
+      setIsLoading(true);
+
+      try{
+        const response = await fetch(`${BASE_URL}/users`, {method : 'get'});
+        const users = (await response.json()) as UserInfo[];
+        setUsers(users);
+      } catch (e : any) {
+        setError(e)
+      } finally{
+        setIsLoading(false);
+      }
+            
+
+      
     };
 
     fetchUsers()
   }, [])
    
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+
+  if(error){
+    return <div>ERORR! Try again</div>
+  }
 
   return (
     

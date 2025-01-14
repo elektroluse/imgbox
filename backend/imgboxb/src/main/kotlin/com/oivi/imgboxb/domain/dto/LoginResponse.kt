a53f4dd670af69f22a5475d9f030a80e24934917
elektroluse <pinkbean@hotmail.no>
2025-01-14 19:20:46 +0100

    token --> accessToken rename

--- backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/domain/dto/LoginResponse.kt
+++ backend/imgboxb/src/main/kotlin/com/oivi/imgboxb/domain/dto/LoginResponse.kt
@@ -3,7 +3,7 @@ package com.oivi.imgboxb.domain.dto
 data class LoginResponse(
     val message : String,
     val authenticated : Boolean,
-    val token : String,
+    val accessToken : String,
     val tokenType : String = " Bearer"
 
 )

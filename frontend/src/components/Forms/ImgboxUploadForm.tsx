a7b9fa7597d1e5536e26fe5c228eee6e47d11c3d
elektroluse <pinkbean@hotmail.no>
2025-01-22 21:40:17 +0100

    upload form logic refactor and fix

--- frontend/src/components/Forms/ImgboxUploadForm.tsx
+++ frontend/src/components/Forms/ImgboxUploadForm.tsx
@@ -14,10 +14,10 @@ import {
 import * as z from "zod"
 import {
   cn
-} from "@/lib/utils"
+} from "../../lib/utils"
 import {
   Button
-} from "@/components/ui/button"
+} from "../../components/ui/button"
 import {
   Form,
   FormControl,
@@ -47,45 +47,59 @@ import {
   FileUploaderItem
 } from "../../components/ui/file-upload"
 
+
+const MAX_FILE_SIZE = 1000000 // 1MB
+const ACCEPTED_IMAGE_TYPES = [
+  "image/png",
+  "image/jpeg",
+  "image/svg+xml",
+  "image/webp"
+]
+
 const formSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().max(2048).optional(),
   tags: z.array(z.string()).nonempty("Please at least one item"),
-  fileInput: z.string()
+  files: z.array(
+    z.instanceof(File).refine((file) => file.size < 4 * 1024 * 1024, {
+      message: "File size must be less than 4MB",
+    }),
+  )
+  .max(5, {
+    message: "Maximum 5 files are allowed",
+  })
+  .nullable(),
 });
-
 export default function ImgboxUploadForm() {
 
-  const [files, setFiles] = useState < File[] | null > (null);
-
+  //const [files, setFiles] = useState < File[] | null > ([]);
+  
   const dropZoneConfig = {
     maxFiles: 1,
-    maxSize: 1024 * 1024 * 4,
+    maxSize: 1024 * 1024 * 4 * 1000,
     multiple: false,
   };
   const form = useForm < z.infer < typeof formSchema >> ({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      "tags": ["test"],
-      "title": "",
-      "description": "",
-      "fileInput" : ""
+      tags: ["test"],
+      title: "",
+      description: "",
+      files : null
+      
     },
   })
 
-  function onSubmit(values: z.infer < typeof formSchema > ) {
+
+   function onSubmit(values: z.infer < typeof formSchema > ) {
     try {
+      
       console.log(values);
-      toast(
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
-        </pre>
-      );
-    } catch (error) {
-      console.error("Form submission error", error);
-      toast.error("Failed to submit the form. Please try again.");
-    }
+     
+    } catch (error) {}
+      
   }
+
 
   return (
     <Form {...form}>
@@ -150,20 +164,24 @@ export default function ImgboxUploadForm() {
         
             <FormField
               control={form.control}
-              name="fileInput"
-              render={({ field }) => (
+              name="files"
+              render={({field}) => (
                 <FormItem>
                   <FormLabel>Select File</FormLabel>
                   <FormControl>
                     <FileUploader
-                      value={files}
-                      onValueChange={setFiles}
+                      value={field.value}
+                      onValueChange={field.onChange}
                       dropzoneOptions={dropZoneConfig}
+                    
                       className="relative bg-background rounded-lg p-2"
                     >
                       <FileInput
-                        id="fileInput"
+                        id="file"
                         className="outline-dashed outline-1 outline-slate-500"
+                              
+
+      
                       >
                         <div className="flex items-center justify-center flex-col p-8 w-full ">
                           <CloudUpload className='text-gray-500 w-10 h-10' />
@@ -175,25 +193,34 @@ export default function ImgboxUploadForm() {
                             SVG, PNG, JPG or GIF
                           </p>
                         </div>
+                  
                       </FileInput>
-                      <FileUploaderContent>
-                        {files &&
-                          files.length > 0 &&
-                          files.map((file, i) => (
-                            <FileUploaderItem key={i} index={i}>
-                              <Paperclip className="h-4 w-4 stroke-current" />
-                              <span>{file.name}</span>
-                            </FileUploaderItem>
-                          ))}
-                      </FileUploaderContent>
+                      <FileUploaderContent className="">
+                      {field.value && field.value.length > 0 && 
+                    
+                      field.value.map((file, i) => (
+                        <FileUploaderItem
+                          key={i}
+                          index={i}>
+                            
+                          <Paperclip className="h-4 w-4 stroke-current" />
+                          <span>{file.name}</span>
+                        
+                        </FileUploaderItem>
+                      ))}
+                  </FileUploaderContent>
                     </FileUploader>
                   </FormControl>
                   <FormDescription>Select a file to upload.</FormDescription>
                   <FormMessage />
                 </FormItem>
+                
+
               )}
+              
             />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" > Submit
+        </Button>
       </form>
     </Form>
   )
--- frontend/src/pages/Upload.tsx
+++ frontend/src/pages/Upload.tsx
@@ -1,3 +1,4 @@
+import ImgboxCreateForm from "../components/Forms/ImgboxCreateForm";
 import ImgboxUploadForm from "../components/Forms/ImgboxUploadForm";
 import Header from "../components/Navbar/Header";
 import { useAuth } from "../services/AuthProvider";
@@ -5,9 +6,10 @@ import { useAuth } from "../services/AuthProvider";
 export function Upload(){
     const auth = useAuth();
     return(
-        <>
+        <main className="bg-slate-50 h-screen w-screen space-y-5">
             <Header/>
             <ImgboxUploadForm />
-        </>
+        </main>
+        
     )
 }
